3f31d59a13ab00a5c4164b087462a18b8ac0afa8
reaperizy <wadi.wahyudin7@gmail.com>
2022-05-25 23:37:53 +0700

    clean code

--- movie/lib/presentation/pages/home_movie_page.dart
+++ movie/lib/presentation/pages/home_movie_page.dart
@@ -1,5 +1,6 @@
 import 'package:about/about.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+//import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'package:search/presentation/pages/search_page.dart';
